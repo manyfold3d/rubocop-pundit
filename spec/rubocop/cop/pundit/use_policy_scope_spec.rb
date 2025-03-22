422a7abb06bbee22b78a37bd20d59619364e989c
Issy Long <me@issylong.com>
2025-03-22 17:31:50 +0000

    Add tests for the `Pundit/UsePolicyScope` cop

--- spec/rubocop/cop/pundit/use_policy_scope_spec.rb
+++ spec/rubocop/cop/pundit/use_policy_scope_spec.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Pundit::MissingPolicyScope, :config do
-  let(:config) { RuboCop::Config.new }
+RSpec.describe RuboCop::Cop::Pundit::UsePolicyScope do
+  let(:config) do
+    RuboCop::Config.new(
+      'Pundit/UsePolicyScope' => {
+        'Enabled' => true
+      }
+    )
+  end
+  subject(:cop) { described_class.new(config) }
 
-  # TODO: Write test code
-  #
-  # For example
-  # it 'registers an offense when using `#bad_method`' do
-  #   expect_offense(<<~RUBY)
-  #     bad_method
-  #     ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
-  #   RUBY
-  # end
+  it 'registers an offense when policy scope is missing' do
+    expect_offense(<<~RUBY)
+      def index
+        @records = Record.all
+                   ^^^^^^^^^^ Wrap model in policy_scope() before using Active Model query methods.
+      end
+    RUBY
+  end
 
-  # it 'does not register an offense when using `#good_method`' do
-  #   expect_no_offenses(<<~RUBY)
-  #     good_method
-  #   RUBY
-  # end
+  it 'does not register an offense when policy scope is used' do
+    expect_no_offenses(<<~RUBY)
+      def index
+        @records = policy_scope(Record).all
+      end
+    RUBY
+  end
 end
